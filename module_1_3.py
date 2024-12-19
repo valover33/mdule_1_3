55dcac06f62c3e6e1f1fc8db41d39947ae79ac90
valover33 <83272190+valover33@users.noreply.github.com>
2024-12-19 20:08:33 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+# Практическая работа по уроку "Динамическая типизация"
+
+name = "Valery"
+print("Name:", name)
+age = 56
+print("Age:", age)
+age = 56 + 1
+print("New_age:", age)
+is_student = True
+print("Is Student:", is_student)
